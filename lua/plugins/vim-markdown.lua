18c903dc0e0f2662a208da815c1e05b03ccd38f5
Séamus O'Sullivan <m0ose01@proton.me>
2024-01-20 22:40:15 +0000

    Enable md extensions for vim markdown

--- lua/plugins/vim-markdown.lua
+++ lua/plugins/vim-markdown.lua
@@ -1,4 +1,10 @@
 return {
 	'godlygeek/tabular',
-	dependencies = {'preservim/vim-markdown'}
+	dependencies = {
+		'preservim/vim-markdown',
+		config = function ()
+			vim.g.vim_markdown_math = 1
+			vim.g.vim_markdown_frontmatter = 1
+		end
+	}
 }
