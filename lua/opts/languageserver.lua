dcdf51c04659fb505157b5c4b47392719afe6a21
Séamus O'Sullivan <m0ose01@proton.me>
2024-01-27 03:32:19 +0000

    Change autocomplete mappings.

--- lua/opts/languageserver.lua
+++ lua/opts/languageserver.lua
@@ -4,8 +4,8 @@ local cmp = require("cmp")
 cmp.setup {
 	snippet = function(args) require("luasnip").lsp_expand(args.body) end,
 	mapping = cmp.mapping.preset.insert {
-	["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-    ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+	["<s-tab>"] = cmp.mapping.select_prev_item(),
+    ["<tab>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
