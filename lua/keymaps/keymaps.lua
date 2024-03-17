136520eccd9ed909e4be856323376c4609e35b03
Seamus <m0ose01@proton.me>
2024-03-17 01:14:10 +0000

    Add citation picker support.

--- lua/keymaps/keymaps.lua
+++ lua/keymaps/keymaps.lua
@@ -46,3 +46,17 @@ map('n', '<leader>fg', telescope.live_grep, {})
 map('v', '<leader>fg', telescope.grep_string, {})
 map('n', '<leader>fb', telescope.buffers, {})
 map('n', '<leader>fh', telescope.help_tags, {})
+
+-- Zotero Citation Picker
+function ZoteroCite()
+	local url = '"http://127.0.0.1:23119/better-bibtex/cayw?format=pandoc&brackets=yes"'
+	local handle = io.popen('curl -s ' .. url)
+	local citekey = handle:read("*a")
+	citekey = citekey:gsub('[\n\r]', ' ')
+	local col = vim.api.nvim_win_get_cursor(0)[2]
+	local row = vim.api.nvim_win_get_cursor(0)[1]
+	vim.api.nvim_buf_set_text(0,row - 1, col, row - 1, col, {citekey})
+	end
+
+map('i', '<A-z>', ZoteroCite)
+map('n', '<leader>zz', ZoteroCite)
