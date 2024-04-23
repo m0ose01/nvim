35d28125de68a9410a9b47e2839dcf8f6a7f544e
Seamus <m0ose01@proton.me>
2024-04-23 23:33:04 +0100

    Add nvim Project
    Add plugin which automatically navigates to project root, using LSP or .git.

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,6 +3,12 @@ return {
 	config = {
 		view = {
 			width = 30,
-		}
+		},
+		sync_root_with_cwd = true,
+		respect_buf_cwd = true,
+		update_focused_file = {
+			enable = true,
+			update_root = true,
+		},
 	}
 }
