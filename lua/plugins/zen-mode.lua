a6dc2144cb6d6bebdf1141982ab660f6eb99bd3a
Seamus <m0ose01@proton.me>
2024-12-18 22:17:41 +0000

    Configure zen-mode.
    Configure zen-mode plugins to change colour scheme, launch on keyboard
    shortcut, remove visual clutter.

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,5 +1,51 @@
 return {
 	"folke/zen-mode.nvim",
-	opts = { },
+	opts = {
+		window = {
+			backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+			-- height and width can be:
+			-- * an absolute number of cells when > 1
+			-- * a percentage of the width / height of the editor when <= 1
+			-- * a function that returns the width or the height
+			width = 120, -- width of the Zen window
+			height = 1, -- height of the Zen window
+			-- by default, no options are changed for the Zen window
+			-- uncomment any of the options below, or add other vim.wo options you want to apply
+			options = {
+				signcolumn = "no", -- disable signcolumn
+				number = false, -- disable number column
+				relativenumber = false, -- disable relative numbers
+				cursorline = false, -- disable cursorline
+				cursorcolumn = false, -- disable cursor column
+				foldcolumn = "0", -- disable fold column
+				-- list = false, -- disable whitespace characters
+			},
+		},
+		plugins = {
+			-- disable some global vim options (vim.o...)
+			-- comment the lines to not apply the options
+			options = {
+				enabled = true,
+				ruler = false, -- disables the ruler text in the cmd line area
+				showcmd = false, -- disables the command in the last line of the screen
+				-- you may turn on/off statusline in zen mode by setting 'laststatus' 
+				-- statusline will be shown only if 'laststatus' == 3
+				laststatus = 0, -- turn off the statusline in zen mode
+			},
+			gitsigns = { enabled = false }, -- disables git signs
+			tmux = { enabled = false }, -- disables the tmux statusline
+		},
+		-- callback where you can add custom code when the Zen window opens
+		on_open = function()
+			vim.cmd.colorscheme("rose-pine-dawn")
+		end,
+		-- callback where you can add custom code when the Zen window closes
+		on_close = function()
+			vim.cmd.colorscheme("onedark")
+		end,
+	},
+	init = function()
+		Map('n', '<leader>zm', require("zen-mode").toggle)
+	end,
 	ft = {"markdown", "md", "qmd", "Rmd", "txt"},
 }
