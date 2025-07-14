276b4d5259376f9f10b10becb8183def49bdf639
Séamus <m0ose01@proton.me>
2025-07-14 11:55:55 +0100

    Add treesitter parsers.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = {
-				"c", "lua", "python", "r", "julia", "matlab", "markdown", "markdown_inline", "html", "css", "javascript", "rust",
+				"c", "lua", "python", "r", "julia", "matlab", "markdown", "markdown_inline", "html", "css", "javascript", "rust", "gotmpl", "glsl",
 			},
 			highlight = {enable = true},
 			indent = {enable = true}
