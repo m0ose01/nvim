e88d45351888059ead9b183838954176a8521429
Séamus O'Sullivan <m0ose01@proton.me>
2026-06-22 01:05:12 +0100

    Replace nvim-dap-ui with nvim-dap-view.
    Replace nvim-dap-ui with nvim-dap view, and change debug configuration
    to stop requiring explicit config. Use launch.json files instead.
    Remove old plugins, update lazy.lock.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,58 +2,53 @@ return {
     "mfussenegger/nvim-dap",
 	dependencies = {
 		{
-			"rcarriga/nvim-dap-ui",
-			dependencies = {
-				"https://github.com/nvim-neotest/nvim-nio"
+			"igorlfs/nvim-dap-view",
+			opts = {
+				winbar = {
+					sections = {
+						"watches",
+						"scopes",
+						"exceptions",
+						"breakpoints",
+						"threads",
+						"repl",
+						"console",
+					}
+				}
 			},
-			opts = {},
 		},
 	},
 	config = function()
 		--Debug Adapter Protocol Setup
 		-- Debug Shortcuts
-		local debug = require("dap")
-		local debugui = require('dapui')
+		local dap = require("dap")
+		local debugui = require("dap-view")
 
-		debug.adapters.debugpy = {
+		dap.set_log_level("DEBUG")
+
+		dap.adapters.debugpy = {
 			type = "server",
 		}
 
-		debug.adapters.lldb = {
+		local lldb_dap_executable = "/usr/bin/lldb-vscode-14"
+		if vim.loop.os_uname().sysname == "Darwin" then
+			lldb_dap_executable = "/Library/Developer/CommandLineTools/usr/bin/lldb-dap"
+		end
+		dap.adapters.lldb = {
 			type = "executable",
-			command = "/usr/bin/lldb-vscode-14",
+			command = lldb_dap_executable,
 			name = "lldb",
 		}
 
-		local lldb = {
-			name = 'Launch lldb',
-			type = 'lldb',
-			request = 'launch',
-			program = function ()
-				return vim.fn.input(
-					"Path to binary: ",
-					vim.fn.getcwd() .. "/",
-					"file"
-				)
-			end,
-			cwd = "${workspaceFolder}",
-			stopOnEntry = false,
-			args = function ()
-				return {vim.fn.input("Args: ")}
-			end,
-			runInTerminal = true,
+		dap.configurations = {
 		}
 
-		debug.configurations = {
-			c = {lldb},
-		}
-
-		Map('n', '<leader>db', debug.toggle_breakpoint)
-		Map('n', '<leader>dB', function() debug.set_breakpoint(vim.fn.input('Condition: ')) end)
-		Map('n', '<leader>dc', debug.continue)
-		Map('n', '<leader>ds', debug.step_over)
-		Map('n', '<leader>di', debug.step_into)
-		Map('n', '<leader>do', debug.step_out)
+		Map('n', '<leader>db', dap.toggle_breakpoint)
+		Map('n', '<leader>dB', function() dap.set_breakpoint(vim.fn.input('Condition: ')) end)
+		Map('n', '<leader>dc', dap.continue)
+		Map('n', '<leader>ds', dap.step_over)
+		Map('n', '<leader>di', dap.step_into)
+		Map('n', '<leader>do', dap.step_out)
 		Map('n', '<leader>dd', debugui.toggle)
 	end,
 	keys = "<leader>dd"
