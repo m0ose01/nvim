e0017f1a6565ba627da7ebaaf94bb747c323a3d7
Séamus O'Sullivan <m0ose01@proton.me>
2026-07-13 20:39:22 +0100

    Fix treesitter.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,16 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"c", "lua", "python", "r", "julia", "matlab", "markdown", "markdown_inline", "html", "css", "javascript", "rust", "gotmpl", "glsl", "c_sharp",
-			},
-			highlight = {enable = true},
-			indent = {enable = true}
+	lazy = false,
+	build=":TSUpdate",
+	opts = {},
+	start = function()
+		require("nvim-treesitter").install({
+			"c", "lua", "python", "r", "julia", "matlab", "markdown", "markdown_inline", "html", "css", "javascript", "rust", "gotmpl", "glsl", "c_sharp",
 		})
+		vim.api.nvim_create_autocmd('FileType', {
+			pattern = { '<filetype>' },
+			callback = function() vim.treesitter.start() end,
+})
 	end,
 	build = ":TSUpdate",
 }
